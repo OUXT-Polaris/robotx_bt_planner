c7a5a1230e2de794e457e80d5b49540af18a63e0
HansRobo <pythagora.yoshimoto@gmail.com>
2020-03-12 01:01:17 +0900

    Implement Component Class

--- include/robotx_bt_planner/bt_planner_component.hpp
+++ include/robotx_bt_planner/bt_planner_component.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, OUXT-Polaris
+// Copyright (c) 2020, OUXT-Polaris
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,34 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-#ifndef ROBOTX_BT_PLANNER__ROBOTX_BT_PLANNER_HPP_
-#define ROBOTX_BT_PLANNER__ROBOTX_BT_PLANNER_HPP_
+#ifndef ROBOTX_BT_PLANNER__BT_PLANNER_COMPONENT_HPP_
+#define ROBOTX_BT_PLANNER__BT_PLANNER_COMPONENT_HPP_
 
 #include <behaviortree_cpp_v3/bt_factory.h>
 #include <behaviortree_cpp_v3/loggers/bt_zmq_publisher.h>
 #include <rclcpp/rclcpp.hpp>
-#include <memory>
+#include <robotx_bt_planner/visibility.h>
 #include <chrono>
+#include <memory>
 
 namespace robotx_bt_planner
 {
-class BehaviorTreePlannerNode : public rclcpp::Node
+class BTPlannerComponent : public rclcpp::Node
 {
 public:
-  BehaviorTreePlannerNode()
-  : rclcpp::Node("robotx_bt_planner")
-  {
-    using std::chrono_literals::operator""ms;
-    // 共有ライブラリからプラグインを読み込み
-    factory.registerFromPlugin(
-      "install/robotx_behavior_tree/lib/robotx_behavior_tree/"
-      "libexample_action.so");
-    // Grootへ実行情報を送信する
-    publisher_zmq = std::make_unique<BT::PublisherZMQ>(tree);
-    timer = create_wall_timer(
-      500ms, std::bind(&BehaviorTreePlannerNode::timerCallback, this));
-  }
+  ROBOTX_BT_PLANNER_PUBLIC
+  explicit BTPlannerComponent(const rclcpp::NodeOptions & options);
 
 private:
   void timerCallback();
@@ -50,4 +39,5 @@ private:
   std::unique_ptr<BT::PublisherZMQ> publisher_zmq;
 };
 }  // namespace robotx_bt_planner
-#endif  // ROBOTX_BT_PLANNER__ROBOTX_BT_PLANNER_HPP_
+
+#endif  // ROBOTX_BT_PLANNER__BT_PLANNER_COMPONENT_HPP_
---
+++ src/bt_planner_component.cpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2020, OUXT-Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <robotx_bt_planner/bt_planner_component.hpp>
+#include <memory>
+
+namespace robotx_bt_planner
+{
+BTPlannerComponent::BTPlannerComponent(const rclcpp::NodeOptions & options)
+: rclcpp::Node("robotx_bt_planner", options)
+{
+  using std::chrono_literals::operator""ms;
+  // 共有ライブラリからプラグインを読み込み
+  factory.registerFromPlugin(
+    "install/robotx_behavior_tree/lib/robotx_behavior_tree/"
+    "libexample_action.so");
+  // Grootへ実行情報を送信する
+  publisher_zmq = std::make_unique<BT::PublisherZMQ>(tree);
+  timer = create_wall_timer(
+    500ms, std::bind(&BTPlannerComponent::timerCallback, this));
+}
+void BTPlannerComponent::timerCallback()
+{
+  tree.root_node->executeTick();
+  RCLCPP_INFO(this->get_logger(), "tick");
+}
+}  // namespace robotx_bt_planner
+
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(robotx_bt_planner::BTPlannerComponent)
