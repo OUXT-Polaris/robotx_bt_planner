08284be0c211d462258a6b5a96bae0ee3b699361
HansRobo <pythagora.yoshimoto@gmail.com>
2020-03-26 15:44:53 +0900

    add debug messages

--- src/bt_planner_component.cpp
+++ src/bt_planner_component.cpp
@@ -12,13 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <robotx_bt_planner/bt_planner_component.hpp>
+#include "robotx_bt_planner/bt_planner_component.hpp"
 #include <memory>
 
 namespace robotx_bt_planner
 {
 
-static const char* xml = R"(
+static const char * xml =
+  R"(
     <root>
         <BehaviorTree>
             <Sequence>
@@ -31,12 +32,16 @@ static const char* xml = R"(
 BTPlannerComponent::BTPlannerComponent(const rclcpp::NodeOptions & options)
 : rclcpp::Node("robotx_bt_planner", options)
 {
-  RCLCPP_INFO(this->get_logger(), "tick");
   using std::chrono_literals::operator""ms;
   // 共有ライブラリからプラグインを読み込み
   factory.registerFromPlugin(
     "/home/hans/ros2_eloquent_ws/install/robotx_behavior_tree/lib/libexample_action.so");
-  std::cout << factory.builders().size() << std::endl;
+  std::cout << "REGISTERED PLUGINS : " << std::endl;
+  std::cout << "=================================" << std::endl;
+  for (auto builder : factory.builders()) {
+    std::cout << "  " << builder.first << std::endl;
+  }
+  std::cout << "=================================" << std::endl;
   tree = factory.createTreeFromText(xml);
   // Grootへ実行情報を送信する
   publisher_zmq = std::make_unique<BT::PublisherZMQ>(tree);
@@ -47,9 +52,8 @@ void BTPlannerComponent::timerCallback()
 {
   RCLCPP_INFO(this->get_logger(), "tick");
   tree.root_node->executeTick();
-
 }
 }  // namespace robotx_bt_planner
 
-#include <rclcpp_components/register_node_macro.hpp>
+#include <rclcpp_components/register_node_macro.hpp>  // NOLINT
 RCLCPP_COMPONENTS_REGISTER_NODE(robotx_bt_planner::BTPlannerComponent)
